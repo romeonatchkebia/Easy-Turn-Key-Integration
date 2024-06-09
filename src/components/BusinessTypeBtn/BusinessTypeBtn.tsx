ad17aa5775aae145b8ae400806d5422d7da81863
romeonatchkebia <121379511+romeonatchkebia@users.noreply.github.com>
2024-06-10 03:44:31 +0400

    border color changed for buttons

--- src/components/BusinessTypeBtn/BusinessTypeBtn.tsx
+++ src/components/BusinessTypeBtn/BusinessTypeBtn.tsx
@@ -4,7 +4,7 @@ const BusinessTypeBtn = ({ title, active, onPress }: BusinessTypeCardProps) => {
   return (
     <button
       onClick={onPress}
-      className={`flex items-center justify-center xl:rounded-xl xl:text-xs xl:leading-[19.2px] min-w-[146.67px] h-[44px] text-sm font-sf leading-[22.4px] border-[1px] border-border ${
+      className={`flex items-center justify-center xl:rounded-xl xl:text-xs xl:leading-[19.2px] min-w-[146.67px] h-[44px] text-sm font-sf leading-[22.4px] xl:border-[1px] xl:border-border ${
         active
           ? "border-b-2 border-active text-active xl:bg-active xl:text-white font-bold pt-0.5 xl:w-[121px]"
           : "text-text-content"
